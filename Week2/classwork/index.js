cbb7a0cee3e721b6c530313de0da337d5c89f3e2
Nolan Yee <nolanyee99@gmail.com>
2025-10-13 12:47:06 -0500

    Week 2 Homework - CSS Resume

--- Week2/classwork/index.js
+++ Week2/classwork/index.js
@@ -1,6 +1,10 @@
 // 1. Write a JavaScript program to find the longest string in a given array
 function findLongestString(arr) {
-  // Your code here
+    let result = "";
+    arr.forEach((element) => {
+        result = element.length > result.length ? element : result;
+    });
+    return result;
 }
 
 // Test for findLongestString
@@ -8,34 +12,39 @@ const testArr1 = ["apple", "banana", "cherry", "date"];
 console.log("Expected: banana, Output:", findLongestString(testArr1));
 
 // 2. Write an arrow function that calculates the area of a rectangle
-const calculateArea = () => {
-  // Your code here
-};
-
+const calculateArea = (width, height) => width * height; // implied return, works when no curly braces
 // Test for calculateArea
 console.log("Expected: 20, Output:", calculateArea(4, 5));
 
 // 3. Write a JavaScript program to display the reading status (i.e. display book name, author name and reading status) of the following books:
 const library = [
-  {
-    title: "The Road Ahead",
-    author: "Bill Gates",
-    readingStatus: true,
-  },
-  {
-    title: "Walter Isaacson",
-    author: "Steve Jobs",
-    readingStatus: false,
-  },
-  {
-    title: "Mockingjay: The Final Book of The Hunger Games",
-    author: "Suzanne Collins",
-    readingStatus: false,
-  },
+    {
+        title: "The Road Ahead",
+        author: "Bill Gates",
+        readingStatus: true,
+    },
+    {
+        title: "Walter Isaacson",
+        author: "Steve Jobs",
+        readingStatus: false,
+    },
+    {
+        title: "Mockingjay: The Final Book of The Hunger Games",
+        author: "Suzanne Collins",
+        readingStatus: false,
+    },
 ];
 
 function displayReadingStatus() {
-  // Your code here
+    library.forEach((book) => {
+        // with dots
+        // console.log(book.title + " by " + book.author + (book.readingStatus ? ", being read" : ", not being read"));
+        const { title, author, readingStatus } = book;
+        const message = `${title} by ${author}. ${
+            readingStatus ? "Is being read" : "Is not being read"
+        }`;
+        console.log(message);
+    });
 }
 
 // Test for displayReadingStatus
@@ -43,7 +52,9 @@ displayReadingStatus();
 
 // 4. Square the value of every element in an array, then print the result
 function squareAndPrint(arr) {
-  // Your code here
+    arr.forEach((num) => {
+      console.log(num ** 2); // exponentiation operator
+    })
 }
 
 // Test for squareAndPrint
@@ -52,7 +63,7 @@ squareAndPrint(testArr2);
 
 // 5. Filter array to numbers greater than 5
 function filterGreaterThanFive(arr) {
-  // Your code here
+    return arr.filter((num) => num > 5);
 }
 
 // Test for filterGreaterThanFive
